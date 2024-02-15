2f9b1737c66db63149e47ef6a963991e0f3836bc
Crimsondragon2002 <91789792+Crimsondragon2002@users.noreply.github.com>
2024-02-15 14:36:05 -0500

    Add files via upload
    Contact us page

---
+++ Contact.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Contact() {
+    return (
+        <div className="App">
+            <h1>Contact Us</h1>
+           
+            <p align="left " style={{ marginLeft: "100px" }}>
+                If you would like to reach out for more information, do not hesitate to contact us. We can be reached at the following email address.<br></br>Our business hours are 9-5 during the week. We will respond within 3-5 business days. 
+               
+            </p>
+            <p align="left " style={{ marginLeft: "100px" }}>
+                
+            </p>
+            <p align = "left "style={{ marginLeft: "100px"}} >
+                   Scrummaster Email: HALLC34@my.erau.edu
+            </p>
+        </div>
+
+    );
+    
+
+}
+
+export default App;
