bee6d56b29d89be38055a0575e8e713d6e6932a6
Crimsondragon2002 <91789792+Crimsondragon2002@users.noreply.github.com>
2024-02-16 15:31:41 -0500

    Add files via upload
    FAQ page

---
+++ FAQ.js
@@ -0,0 +1,54 @@
+
+function FAQ() {
+  return (
+    <div className="FAQ">
+       <h1 align ="center">FAQ</h1>
+           
+            <p align="left " style={{ marginLeft: "100px" }}>
+              <b> Question 1.</b>
+              <br>
+              </br>
+              <text style={{ marginLeft:"75px" }}>
+              Answer 1.
+              </text>
+              
+          </p>
+
+            <p align="left " style={{ marginLeft: "100px" }}>
+              <b> Question 2.</b>
+              <br>
+              </br>
+              <text style={{ marginLeft: "75px" }}>
+                  Answer 2.
+              </text>
+            </p>
+            <p align = "left "style={{ marginLeft: "100px"}} >
+              <b> Question 3.</b>
+              <br>
+              </br>
+              <text style={{ marginLeft: "75px" }}>
+                  Answer 3.
+              </text>
+          </p>
+          <p align="left " style={{ marginLeft: "100px" }} >
+              <b> Question 4.</b>
+              <br>
+              </br>
+              <text style={{ marginLeft: "75px" }}>
+                  Answer 4.
+              </text>
+          </p>
+          <p align="left " style={{ marginLeft: "100px" }} >
+              <b> Question 5.</b>
+              <br>
+              </br>
+              <text style={{ marginLeft: "75px" }}>
+                  Answer 5.
+              </text>
+          </p>
+        
+    </div>
+  );
+}
+
+export default FAQ;
