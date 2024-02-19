d89c902b9706c69744a68e200a2144c69fb626a9
Caleb Hall <93002893+Caleb-Hall-1015@users.noreply.github.com>
2024-02-18 20:50:55 -0500

    Delete AutoPen.py

--- AutoPen.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-    
-def run_wmap(process,targetURL):
-    commands = f'msfconsole\n load wmap\n wmap_sites -a {targetURL}\n wmap_targets -t {targetURL}\n wmap_run -e\n wmap_vulns -l\n  vulns\n'
-    stdout, stderr = process.communicate(commands)
-    print("Output:\n", stdout)
-        
-process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-run_wmap(process, "http://localhost/DVWA/index.php")
