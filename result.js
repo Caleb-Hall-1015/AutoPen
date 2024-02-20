24833996444f876201fd7227b94c8b7cabc31991
Caleb Hall <93002893+Caleb-Hall-1015@users.noreply.github.com>
2024-02-20 10:41:18 -0500

    Add files via upload

---
+++ result.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function result() {
+    return (
+        <div className="Result">
+            <h1>Results</h1>
+                <h2>Scan History</h2>
+                <p>Select a previous scan to show results</p>
+        </div>
+    );
+}
+
+export default result;
