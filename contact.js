637411e8981e8aa0adb4cb4a4a6961de0f4797e6
Caleb Hall <93002893+Caleb-Hall-1015@users.noreply.github.com>
2024-02-20 10:35:19 -0500

    Update and rename Contact.js to contact.js

--- contact.js
+++ contact.js
@@ -1,23 +1,22 @@
-function Contact() {
+import React from 'react';
+
+function contact() {
     return (
         <div className="Contact">
             <h1>Contact Us</h1>
            
             <p align="left " style={{ marginLeft: "100px" }}>
-                If you would like to reach out for more information, do not hesitate to contact us. We can be reached at the following email address.<br></br>Our business hours are 9-5 during the week. We will respond within 3-5 business days. 
+                If you would like to reach out for more information, do not hesitate to contact us. We can be reached at the following email address.<br></br>Our business hours are 9-5 during the week, and we will respond within 3-5 business days. 
                
             </p>
             <p align="left " style={{ marginLeft: "100px" }}>
                 
             </p>
             <p align = "left "style={{ marginLeft: "100px"}} >
-                   Scrummaster Email: HALLC34@my.erau.edu
+                   Email us at customerservice@autopentest.net
             </p>
         </div>
-
     );
-    
-
 }
 
-export default Contact;
+export default contact;
